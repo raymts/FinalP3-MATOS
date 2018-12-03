54827f9fb4bfcef373d5a7dbd948ac6f81e910e2
Brandor Medina <brandormedina16@gmail.com>
2018-12-03 14:57:18 -0400

    Login change 1

---
+++ Final/Final/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Final.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login ()
+        {
+            return View();
+        }
+
+        // GET: Login/Details/5
+        public ActionResult Login (string user, string pass)
+        {
+            if("admin".Equals(user) && "123".Equals(pass))
+            {
+
+            }
+            return View();
+        }
+      
+    }
+}
---
+++ Final/Final/Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Final.Models
+{
+    public class Login
+    {
+        private String User { get; set; }
+        private String Pass { get; set; }
+        private String select { get; set; }
+
+    }
+}
